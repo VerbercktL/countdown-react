f8796461a12121a719e73a3a38113bb22cd153b5
VerbercktL <lennverberckt@hotmail.com>
2024-01-04 14:40:56 +0100

    new

--- src/Components/Introtext.js
+++ src/Components/Introtext.js
@@ -7,6 +7,12 @@ text-align:center;
 font-family:Arial;
 font-size: 4rem;`
 
+const H2 = styled.h2`
+color: #011F26;
+text-align:center;
+font-family:Arial;
+font-size: 3rem;`
+
 
 const P = styled.p`
 color: #011F26;
@@ -15,9 +21,15 @@ font-family:Arial;
 font-size: 2rem;`
 
 function Introtext() {
+  const today = new Date();
+  const month = today.getMonth()+1;
+  const year = today.getFullYear();
+  const date = today.getDate();
+  const currentDate = date + "/" + month + "/" + year;
     return (
       <div>
-        <H1>New Year countdown</H1>
+        <H1>Welcome, </H1>
+        <H2>We are today {currentDate}</H2>
         <P>2025 is almost there, but you still have to wait:</P>
       </div>
     );
