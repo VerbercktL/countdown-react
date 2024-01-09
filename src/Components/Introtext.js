2bb693ea116154e21e28a0142840c7aff7334947
VerbercktL <lennverberckt@hotmail.com>
2024-01-09 12:35:11 +0100

    changes

--- src/Components/Introtext.js
+++ src/Components/Introtext.js
@@ -21,15 +21,9 @@ function Introtext() {
   const [hours, setHours] = useState();
   const [minutes, setMinutes] = useState();
   const [seconds, setSeconds] = useState();
-  const handleChange = (event) => {
-    const name = event.target.name;
-    const value = event.target.value;
-    setInputs(values => ({...values, [name]: value}))
-    setDays(days);
-    setHours(hours);
-    setMinutes(minutes);
-    setSeconds(seconds);
-  }
+  const [inputDate, setInputDate] = useState("1 Jan 2025");
+  const [currentDate, setCurrentDate] = useState(inputDate);
+
 
   const handleNameChange = (event) => {
     const name = event.target.name;
@@ -40,6 +34,14 @@ function Introtext() {
   const handleSubmit = (event) => {
     event.preventDefault();
   }
+  const onChangeHandler = (event) => {
+    setInputDate(event.target.value);
+  };
+
+  const onClickHandler = () => {
+    setCurrentDate(inputDate);
+  };
+
     return (
       <div>
         <H1>Countdown</H1>
@@ -53,31 +55,10 @@ function Introtext() {
         onChange={handleNameChange}
       />
       </label>
-      {/*<<label>Day:
-        <input 
-          type="number" 
-          name="day" 
-          value={inputs.day || ""} 
-          onChange={handleChange}
-        />
-        </label>
-        <label>Month:
-        <input 
-          type="number" 
-          name="month" 
-          value={inputs.month || ""} 
-          onChange={handleChange}
-        />
-        </label>
-        <label>Year:
-       <input 
-          type="number" 
-          name="year" 
-          value={inputs.year || ""} 
-          onChange={handleChange}
-        />
-    </label>*/}
-        <button onClick={() => setEventText(inputs.name)}>Start</button>
+      <input className="countdown-input" onChange={onChangeHandler} />
+      <button className="countdown-button" onClick={onClickHandler}>
+          Countdown
+        </button>
     </form>
       </div>
     );
